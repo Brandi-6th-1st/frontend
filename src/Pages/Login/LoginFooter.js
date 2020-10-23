d551843146d6ce782a64349fe8c4ee720cfaab92
나지은 <najieun@najieun-ui-MacBookPro.local>
2020-10-23 10:13:12 +0900

    Add: 회원가입페이지 footer와 content겹침 방지를 위한 div 추가

--- src/Pages/Login/LoginFooter.js
+++ src/Pages/Login/LoginFooter.js
@@ -1,23 +1,26 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import styled from 'styled-components';
 
 export default function LoginFooter() {
   return (
-    <Footer>
-      <FooterInner>
-        <CompanyInfo>
-          회사명 : (주)브랜디 | 주소 : (06223) 서울특별시 강남구 테헤란로 32길
-          26 청송빌딩 | 사업자등록번호 : 220-88-93187 I 통신판매업신고 :
-          2016-서울강남-00359호
-        </CompanyInfo>
-        <CompanyInfo>
-          이메일 : help@brandi.co.kr | 2018 © brandi inc.
-        </CompanyInfo>
-        <Policy>
-          <a>이용약관</a> | <a>개인정보처리방침</a>
-        </Policy>
-      </FooterInner>
-    </Footer>
+    <Fragment>
+      <FooterFantom></FooterFantom>
+      <Footer>
+        <FooterInner>
+          <CompanyInfo>
+            회사명 : (주)브랜디 | 주소 : (06223) 서울특별시 강남구 테헤란로 32길
+            26 청송빌딩 | 사업자등록번호 : 220-88-93187 I 통신판매업신고 :
+            2016-서울강남-00359호
+          </CompanyInfo>
+          <CompanyInfo>
+            이메일 : help@brandi.co.kr | 2018 © brandi inc.
+          </CompanyInfo>
+          <Policy>
+            <a>이용약관</a> | <a>개인정보처리방침</a>
+          </Policy>
+        </FooterInner>
+      </Footer>
+    </Fragment>
   );
 }
 
@@ -30,6 +33,15 @@ const Footer = styled.div`
   bottom: 0;
   width: 100%;
   background-color: #212121;
+`;
+
+const FooterFantom = styled.div`
+  display: block;
+  padding: 20px 0;
+  width: 100%;
+  height: 100px;
+  /* background-color: #212121; */
+  border: 1px solid red;
 `;
 
 const FooterInner = styled.div`
