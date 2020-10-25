fbc6294edfb23ee2f0cb99f88331b121888b75db
나지은 <najieun@najieun-ui-MacBookPro.local>
2020-10-25 11:31:48 +0900

    Add: route 추가

--- src/Pages/User/User.js
+++ src/Pages/User/User.js
@@ -1,6 +1,28 @@
 import React from 'react';
 import styled from 'styled-components';
+import TimePicker from 'react-time-picker';
+import Nav from '../../Components/Nav/Nav';
+import { AiOutlineHome } from 'react-icons/ai';
 
 export default function User() {
-  return <div></div>;
+  return (
+    <Container>
+      <Nav />
+      <PageTitle>
+        셀러정보관리 <small>셀러 정보 조회 / 수정</small>
+      </PageTitle>
+      <PageBar>
+        <AiOutlineHome />
+      </PageBar>
+      <TimePicker />
+    </Container>
+  );
 }
+
+const Container = styled.div``;
+
+const PageTitle = styled.div`
+  color: #666;
+`;
+
+const PageBar = styled.div``;
--- src/Routes.js
+++ src/Routes.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import React from 'react';
 import Login from './Pages/Login/Login';
 import Signup from './Pages/Signup/Signup';
-import Signup2 from './Pages/Signup/Signup2';
+import User from './Pages/User/User';
 import Main from './Pages/Main/Main';
 
 function Routes() {
@@ -11,7 +11,7 @@ function Routes() {
       <Switch>
         <Route exact path="/Login" component={Login} />
         <Route exact path="/Signup" component={Signup} />
-        <Route exact path="/Signup2" component={Signup2} />
+        <Route exact path="/User" component={User} />
         <Route exact path="/" component={Main} />
       </Switch>
     </Router>
