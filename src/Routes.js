02ead9297ef534bf0d6e91d4ba99067b7e0ebe61
JongilSohn <sji7532@gmail.com>
2020-11-08 18:52:20 +0900

    주석 추가

--- src/Routes.js
+++ src/Routes.js
@@ -4,7 +4,6 @@ import Login from './Pages/Login/Login';
 import Signup from './Pages/Signup/Signup';
 import Main from './Pages/Main/Main';
 import ProductManagement from './Pages/Product/ProductManagement';
-import ProductDetailManage from './Pages/ProductDetailManage/ProductDetailManage';
 import Home from './Pages/Home/Home';
 import Order from './Pages/Order/Order';
 
@@ -17,7 +16,6 @@ function Routes() {
         <Route exact path="/main" component={Main} />
         <Route exact path="/home" component={Home} />
         <Route exact path="/product" component={ProductManagement} />
-        <Route exact path="/product_detail" component={ProductDetailManage} />
         <Route exact path="/order" component={Order} />
         <Route exact path="/order/:id" component={Order} />
       </Switch>
